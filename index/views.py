cd87305f3883ceec8c47f354145e40b095fac621
Kamich-GP <yurochkagameplus@gmail.com>
2025-02-18 21:28:38 +0500

    urls, views

--- index/views.py
+++ index/views.py
@@ -3,6 +3,7 @@ from .models import Category, Product
 
 
 # Create your views here.
+# Главная страница
 def home_page(request):
     # Достаем данные из БД
     categories = Category.objects.all()
@@ -12,3 +13,26 @@ def home_page(request):
     context = {'products': products, 'categories': categories}
 
     return render(request, 'home.html', context)
+
+
+# Вывод информации о конкретном продукте
+def product_page(request, pk):
+    # Достаем данные из БД
+    product = Product.objects.get(id=pk)
+
+    # Отправляем данные на фронт
+    context = {'product': product}
+
+    return render(request, 'product.html', context)
+
+
+# Вывод товаров по категории
+def category_page(request, pk):
+    # Достаем данные из БД
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
+
+    # Передаем данные на фронт
+    context = {'category': category, 'products': products}
+
+    return render(request, 'category.html', context)
