c632061f760b7b1b437e287fe4c79b9b73a742ff
Kamich-GP <yurochkagameplus@gmail.com>
2025-02-25 21:40:14 +0500

    forms

--- index/views.py
+++ index/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Product
+from .forms import RegForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.views import View
 
 
 # Create your views here.
@@ -36,3 +40,49 @@ def category_page(request, pk):
     context = {'category': category, 'products': products}
 
     return render(request, 'category.html', context)
+
+
+# Поиск товара
+def search_product(request):
+    if request.method == "POST":
+        get_product = request.POST.get('search_product')
+
+        searched_products = Product.objects.filter(product_name__iregex=get_product)
+
+        if searched_products:
+            context = {'products': searched_products}
+
+            return render(request, 'result.html', context)
+        else:
+            return redirect('/')
+
+
+# Регистрация
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': RegForm}
+
+        return render(request, self.template_name, context)
+
+
+    def post(self, request):
+        form = RegForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password2')
+
+            user = User.objects.create_user(username=username,
+                                     email=email,
+                                     password=password)
+            user.save()
+
+            login(request, user)
+
+            return redirect('/')
+
+
+
